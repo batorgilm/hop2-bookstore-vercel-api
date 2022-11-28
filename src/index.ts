6c50574479f9e67173dbe7e115e180825947ad3e
batorgilm <batorgil.m@nest.edu.mn>
2022-11-28 16:27:58 +0800

    test endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import routes from "./routes";
 
-const app = express()
-const port = process.env.PORT || 8080
-
-app.get('/', (_req: Request, res: Response) => {
-	return res.send('Express Typescript on Vercel')
-})
-
-app.get('/ping', (_req: Request, res: Response) => {
-	return res.send('pong 🏓')
-})
-
+const app = express();
+const port = process.env.PORT || 8080;
+const uri =
+  "mongodb+srv://admin:admin123@hop-2.xrzll2f.mongodb.net/?retryWrites=true&w=majority";
 app.listen(port, () => {
-	return console.log(`Server is listening on ${port}`)
-})
+  console.log(`Server is listening on ${port}`);
+  try {
+    mongoose.connect(uri, {}).then(() => {
+      console.log("Connected to DB");
+    });
+  } catch (error) {
+    console.error("Could not connect to DB");
+    process.exit(1);
+  }
+
+  routes(app);
+});
---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+import { Response, Request, Express } from "express";
+
+const routes = (app: Express) => {
+  app.get("/", (req: Request, res: Response) => {
+    res.send({
+      status: 200,
+      data: {
+        msg: "success",
+      },
+    });
+  });
+};
+export default routes;
